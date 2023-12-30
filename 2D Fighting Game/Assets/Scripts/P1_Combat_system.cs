626917b5868629ff8a63d50ddcec935762ddb615
alexgavr2692 <alexgavr007@hotmail.com>
2023-12-30 19:39:57 +0200

    ypotithetai combat

---
+++ 2D Fighting Game/Assets/Scripts/P1_Combat_system.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    public static CombatManager instance;
+
+    private void Awake()
+    {
+       instance=this; 
+    }
+    public bool CanReceiveInput;
+    public bool InputReceived;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Attack(){
+
+        if(Input.GetButtonDown("P1_Light"))
+        {
+            if(CanReceiveInput)
+            {
+                InputReceived=true;
+                CanReceiveInput=false;
+            }
+        }else
+        {
+            return;
+        }
+    }
+    public void InputManager(){
+        if(!CanReceiveInput)
+        {
+            CanReceiveInput=true;
+        }else
+        {
+            CanReceiveInput=false;
+        }
+    }
+}
